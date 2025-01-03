e12c2988d932ac6574ae9d0ad372581e1bd57cb2
risav-pyakurel <pyakurel.risav@gmail.com>
2025-01-03 22:37:02 +0545

    changing images

--- carts/views.py
+++ carts/views.py
@@ -35,26 +35,22 @@ def add_cart(request, product_id):
             cart = cart, 
         )
         cart_item.save()
-    return HttpResponse(cart_item.product)
-    exit()
 
     return redirect('cart')
         
     
 
-def cart(request,total = 0, cart_item = None):
-    try:
-        cart = Cart.objects.get(cart_id = _cart_id(request))
-        cart_items = CartItem.objects.filter(cart=cart, is_active = True)
-        for cart_item in cart_items:
-            total += (cart_item.product.price * cart_item.quantity)
-            quantity += cart_item.quantity
-    except ObjectNotExist:
-        pass
-    
-    context = {
-        'total' : total,
-        'quantity':quantity,
-        'cart_items' : cart_items,
-    }
-    return render(request,'store/cart.html', context)
+def cart(request,total = 0, quantity= 0, cart_item = None):
+
+            cart = Cart.objects.get(cart_id = _cart_id(request))
+            cart_items = CartItem.objects.filter(cart=cart, is_active = True)
+            for cart_item in cart_items:
+                total += (cart_item.product.price * cart_item.quantity)
+                quantity += cart_item.quantity
+        
+            context = {
+            'total' : total,
+            'quantity':quantity,
+            'cart_items' : cart_items,
+        }
+            return render(request,'store/cart.html', context)
